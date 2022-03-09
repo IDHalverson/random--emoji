d58ea5ff09d24b21eeeba34748fe41e6eb05c3e1
Ian Halverson <halversoncode@gmail.com>
2022-03-08 19:11:17 -0500

    add explicit port

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const path = require("path");
 app.use(express.static('public'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+app.set('port', (process.env.PORT || 3000));
 
 const randomChoice = (list) => {
     return list[Math.floor(Math.random()*list.length)];
@@ -33,6 +34,6 @@ app.get("/emoji", (req, res) => {
     });
 })
 
-const server = app.listen(3000, () => {
+const server = app.listen(process.env.PORT || 3000, () => {
     console.log("App is running.")
 })
