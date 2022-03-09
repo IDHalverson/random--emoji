3aaf896b6e7c758570179b9e9cf6d3f850ea77c4
Ian Halverson <halversoncode@gmail.com>
2022-03-08 19:22:18 -0500

    get rid of console logs

--- index.js
+++ index.js
@@ -13,24 +13,15 @@ const randomChoice = (list) => {
 }
 
 app.get("/emoji", (req, res) => {
-
     const fileContents = fs.readFileSync("emoji_list.txt").toString();
     const oneEmoji = randomChoice(Array.from(fileContents.match(/:[a-zA-Z0-9\-\+\_]+:/g)));
-    console.log("chose:", oneEmoji);
     const filesInDir = fs.readdirSync("public/emojis");
     const oneEmojiFileNameMainPart = oneEmoji.replace(/^:/, "").replace(/:$/, "");
     const filename = filesInDir.find(filename => filename.match(new RegExp(`^${oneEmojiFileNameMainPart}.`)));
-    console.log("filename is", filename)
-
-    // const htmlContent = fs.readFileSync("public/index.html").toString();
-
-    // const newHtmlContent = htmlContent.replace(`img src=""`, `img src="emojis/${filename}"`).replace("<h1></h1>", `<h1>${oneEmoji}</h1>`);
-
-    // fs.writeFileSync("public/dynamic-index.html", newHtmlContent);
-
     res.render("index", {
         emojiPath: `emojis/${filename}`,
-        emojiName: oneEmoji
+        emojiName: oneEmoji,
+        emojiNameRaw: oneEmojiFileNameMainPart
     });
 })
 
